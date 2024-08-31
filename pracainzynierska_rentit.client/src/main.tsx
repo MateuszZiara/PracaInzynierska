50ccc79b301cf32a3e477f44d83b711d484b0ac9
Mateusz Ziara <ziaramateusz@gmail.com>
2024-08-31 22:59:58 +0200

    Initial Commit

---
+++ pracainzynierska_rentit.client/src/main.tsx
@@ -0,0 +1,30 @@
+import '@mantine/core/styles.css';
+// @ts-ignore
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from 'react-router-dom';
+import { MantineProvider, createTheme } from '@mantine/core';
+import { Home } from './Home/Home';
+
+const theme = createTheme({
+
+    primaryColor: 'gray',
+    focusRing: 'always',
+    defaultRadius: 'md',
+    defaultGradient: { from: 'blue', to: 'red', deg: 45 }
+});
+// Routing
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <MantineProvider theme={theme} forceColorScheme="light">
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    </MantineProvider>
+);
