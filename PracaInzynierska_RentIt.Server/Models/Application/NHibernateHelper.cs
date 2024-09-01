300f25ec96c10d75351a26a4bbb3fe2dea4ffcc1
Mateusz Ziara <ziaramateusz@gmail.com>
2024-09-01 15:26:34 +0200

    Create Interaface and NHibernate configuration

---
+++ PracaInzynierska_RentIt.Server/Models/Application/NHibernateHelper.cs
@@ -0,0 +1,36 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Tool.hbm2ddl;
+
+namespace PracaInzynierska_RentIt.Server.Models.Application;
+
+public class NHibernateHelper
+{
+    private static ISessionFactory _sessionFactory;
+
+    public static NHibernate.ISession OpenSession()
+    {
+        return SessionFactory.OpenSession();
+    }
+
+    private static ISessionFactory SessionFactory
+    {
+        get
+        {
+            if (_sessionFactory == null)
+            {
+                _sessionFactory = Fluently.Configure()
+                    .Database(
+                        MsSqlConfiguration.MsSql2012.ConnectionString(
+                            "Server=localhost\\SQLEXPRESS;Database=RentIt;Integrated Security=SSPI;Application Name=RentIt;TrustServerCertificate=true;")
+                    ) 
+                    //.Mappings(m =>
+                     //   m.FluentMappings.AddFromAssemblyOf<AspNetUsers>())
+                    .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true))
+                    .BuildSessionFactory();
+            }
+            return _sessionFactory;
+        }
+    }
+}
