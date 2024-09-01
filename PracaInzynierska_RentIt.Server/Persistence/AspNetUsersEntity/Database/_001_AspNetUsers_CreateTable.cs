7046c9e621f64fed693fb9c57fa41c162bf71512
Mateusz Ziara <ziaramateusz@gmail.com>
2024-09-01 15:59:41 +0200

    Migrations

---
+++ PracaInzynierska_RentIt.Server/Persistence/AspNetUsersEntity/Database/_001_AspNetUsers_CreateTable.cs
@@ -0,0 +1,44 @@
+﻿using FluentMigrator;
+using PracaInzynierska_RentIt.Server.Models.AspNetUsersEntity;
+namespace RentIt_PracaInzynierska.Server.Persistence.AspNetUsersEntity.Database;
+[Migration(001)]
+public class _001_AspNetUsers_CreateTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("AspNetUsers")
+            .WithColumn("Id").AsString().NotNullable().PrimaryKey()
+            .WithColumn("UserName").AsString().NotNullable()
+            .WithColumn("NormalizedUserName").AsString().NotNullable()
+            .WithColumn("Email").AsString().NotNullable()
+            .WithColumn("NormalizedEmail").AsString().NotNullable()
+            .WithColumn("EmailConfirmed").AsBoolean().NotNullable()
+            .WithColumn("PasswordHash").AsString().NotNullable()
+            .WithColumn("SecurityStamp").AsString().Nullable()
+            .WithColumn("ConcurrencyStamp").AsString().Nullable()
+            .WithColumn("PhoneNumber").AsString().Nullable().Nullable()
+            .WithColumn("PhoneNumberConfirmed").AsBoolean().Nullable()
+            .WithColumn("TwoFactorEnabled").AsBoolean().Nullable()
+            .WithColumn("LockoutEnd").AsDateTimeOffset().Nullable()
+            .WithColumn("LockoutEnabled").AsBoolean().Nullable()
+            .WithColumn("AccessFailedCount").AsInt32().Nullable()
+            //Custom
+            .WithColumn(nameof(AspNetUsers.FirstName)).AsString().Nullable()
+            .WithColumn(nameof(AspNetUsers.LastName)).AsString().Nullable()
+            .WithColumn(nameof(AspNetUsers.Name)).AsString().Nullable()
+            .WithColumn(nameof(AspNetUsers.Provider)).AsString().Nullable()
+            .WithColumn(nameof(AspNetUsers.Avatar)).AsBoolean().Nullable()
+            .WithColumn(nameof(AspNetUsers.CreateBy)).AsString().NotNullable()
+            .WithColumn(nameof(AspNetUsers.CreateTime)).AsDateTime().NotNullable()
+            .WithColumn(nameof(AspNetUsers.ModifiedBy)).AsString().Nullable()
+            .WithColumn(nameof(AspNetUsers.ModifiedTime)).AsDateTime().Nullable()
+            .WithColumn(nameof(AspNetUsers.Admin)).AsBoolean().NotNullable();
+    }
+    public override void Down()
+    {
+        if (Schema.Table("AspNetUsers").Exists())
+        {
+            Delete.Table("AspNetUsers");
+        }
+    }
+}
---
+++ PracaInzynierska_RentIt.Server/Persistence/AspNetUsersEntity/Database/_003_AspNetUserClaims_CreateTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace RentIt_PracaInzynierska.Server.Persistence.AspNetUsersEntity.Database;
+[Migration(003)]
+public class _003_AspNetUserClaims_CreateTable : Migration
+{
+    public override void Up()
+    {
+        Create.Table("AspNetUserClaims")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("UserId").AsString(128).NotNullable()
+            .WithColumn("ClaimType").AsString(256).NotNullable()
+            .WithColumn("ClaimValue").AsString(256).NotNullable();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("AspNetUserClaims");
+    }
+}
